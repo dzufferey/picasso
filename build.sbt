72d1c59e05297fac864a1c3f6e3c1b1fd8c91c0f
p011u01 <p011u01@87282681-9773-4243-a559-462cb349d9ff>
2011-12-20 05:24:57 +0000

    git-svn-id: https://svn.ist.ac.at/projects/p011/svn/picasso/trunk@65 87282681-9773-4243-a559-462cb349d9ff

--- build.sbt
+++ build.sbt
@@ -8,8 +8,8 @@ scalaVersion := "2.9.1"
 
 libraryDependencies ++=  Seq(
     "org.scalatest" % "scalatest_2.9.1" % "1.6.1" % "test",
-    "org.scala-lang" % "scala-compiler" % "2.9.1"
-    //"org.sat4j" % "org.sat4j.core" % "2.3.1"
+    "org.scala-lang" % "scala-compiler" % "2.9.1",
+    "org.sat4j" % "org.sat4j.core" % "2.3.1"
 )
 
 mainClass in (Compile, packageBin) := Some("picasso.frontend.basic.Main")
--- runBasic.sh
+++ runBasic.sh
@@ -2,7 +2,7 @@
 export JAVA_OPTS="-Xmx1536M -Xms256M -Xss64M"
 scala_version=scala-2.9.1
 BASEDIR=`dirname $0`
-cp="$HOME/.sbt/boot/$scala_version/lib/*:$BASEDIR/target/${scala_version}/classes:$BASEDIR/lib/*"
+cp="$HOME/.sbt/boot/$scala_version/lib/*:$BASEDIR/target/${scala_version}/classes:$HOME/.ivy2/cache/org.sat4j/org.sat4j.core/jars/*"
 
 #normal version
 exec java -cp ${cp} picasso.frontend.basic.Main $*
