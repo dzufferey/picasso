45db39078d3caf06f011898a23c655904626c36d
p011u01 <p011u01@87282681-9773-4243-a559-462cb349d9ff>
2012-07-24 22:01:22 +0000

    git-svn-id: https://svn.ist.ac.at/projects/p011/svn/picasso/trunk@215 87282681-9773-4243-a559-462cb349d9ff

--- core/src/main/scala/picasso/analysis/InterfaceExtraction.scala
+++ core/src/main/scala/picasso/analysis/InterfaceExtraction.scala
@@ -34,14 +34,26 @@ class InterfactExtraction[P <: DBCT](proc: DepthBoundedProcess[P], cover: Downwa
    * 3rd step: structure the output ...
    */
 
+  /** Checks whether the system respects the assumption needed for the interface extraction. */
+  def conforms: Boolean = {
+    //TODO
+    //method names
+    //predicate / object names in the graph
+    //type of transition
+    //...
+    sys.error("TODO")
+  }
+
   val tg: EdgeLabeledDiGraph[TG[P]] = TransitionsGraphFromCover(proc, cover)
 
-  protected def isObj(node: P#V) = node.state.toString.head.isUpper //TODO this is an HACK!
+  protected def typeOf(node: P#V) = node.state.toString
+
+  protected def isObj(node: P#V) = typeOf(node).head.isUpper //TODO this is an HACK!
 
   protected def isPred(node: P#V) = !isObj(node)
 
   protected def predValue(p: P#V): (String, Boolean) = {
-    val nme = p.state.toString
+    val nme = typeOf(p)
     if (nme startsWith "not_") (nme.substring(4), true)
     else (nme, false)
   }
@@ -57,7 +69,7 @@ class InterfactExtraction[P <: DBCT](proc: DepthBoundedProcess[P], cover: Downwa
 
     val objsWithField = objs.groupBy(o => {
       graph.outEdges(node).find{ case (k, v) => v contains o }.get._1
-    }).map{ case (k, v) => (k.toString, v map (_.state.toString)) } 
+    }).map{ case (k, v) => (k.toString, v map typeOf) } 
 
     val (unaryPreds, binaryPreds) = preds.partition( p => {
       objs.forall(o => !graph(o).contains(p) ) //unary is pointed only by the node itself
@@ -65,22 +77,41 @@ class InterfactExtraction[P <: DBCT](proc: DepthBoundedProcess[P], cover: Downwa
 
     val unaryValues = unaryPreds.map(predValue).toMap
 
-
     val binaryValues = binaryPreds.map( p => {
       val pointedBy = objs.filter(o => !graph(o).contains(p) )
       assert(pointedBy.size == 1)
       val other = pointedBy.head
       val field = graph.outEdges(node).find{ case (k, v) => v contains other }.get._1
       val (pName, v) = predValue(p)
-      (pName, (other.state.toString, v))
+      (pName, (typeOf(other), v))
     }).groupBy(_._1).map{ case (k, v) => (k, v.map(_._2).toMap ) }
 
-    (node.state.toString, objsWithField, unaryValues, binaryValues)
+    (typeOf(node), objsWithField, unaryValues, binaryValues)
+  }
+
+  protected def extractDPV(graph: DepthBoundedConf[P], node: P#V): DPV = {
+    val keep = graph(node) + node
+    val restricted = graph filterNodes keep
+    //flatten to keep a single object.
+    val height = node.depth
+    if (height > 0) {
+      val withLower  = restricted.vertices.map(v => (v, (v /: (0 until math.max(0, v.depth - height)))( (v,_) => v-- ) ) )
+      val morphing = withLower.toMap[P#V,P#V]
+      (morphing(node), restricted morph morphing)
+    } else {
+      (node, restricted)
+    }
   }
 
   protected lazy val eqClassesInGraph: Set[DPV] = {
     //get every object in every "non transient location" and trim the graph so that only the eq class is left.
-    //then remove the duplicates
+    //a non transient location is a location of the cover (not all the location in tg).
+    val objs = cover.flatMap( graph => {
+      val objsNode = graph.vertices.filter(isObj)
+      objsNode.map( n => extractDPV(graph, n) )
+    })
+    //TODO remove the duplicates (isomorphism ...)
+    //first group by the node label
     sys.error("TODO")
   }
 
