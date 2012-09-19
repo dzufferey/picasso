74ca4483a60a4ba50eac195435d6cbef0e811825
p011u01 <p011u01@87282681-9773-4243-a559-462cb349d9ff>
2012-09-19 17:41:29 +0000

    git-svn-id: https://svn.ist.ac.at/projects/p011/svn/picasso/trunk@238 87282681-9773-4243-a559-462cb349d9ff

--- core/src/main/scala/picasso/utils/tools/princess/Printer.scala
+++ core/src/main/scala/picasso/utils/tools/princess/Printer.scala
@@ -6,6 +6,19 @@ import java.io._
 
 object Printer {
 
+  protected def unary(i: InterpretedFct) = i match {
+    case Not => true
+    case _ => false
+  }
+
+  protected def binary(i: InterpretedFct) = i match {
+    case Implies => true 
+    case Or | And | Not => false 
+    case Eq | Neq | Geq | Leq | Gt | Lt => true
+    case Plus | Minus | Times => false
+    case other => Logger.logAndThrow("princess", LogError, "don't know the priority of " + other)
+  }
+
   protected def priority(i: InterpretedFct) = i match {
     case Implies => 0
     case Or => 1
@@ -15,6 +28,23 @@ object Printer {
     case Plus | Minus => 5
     case Times => 6
     case other => Logger.logAndThrow("princess", LogError, "don't know the priority of " + other)
+  }
+
+  protected def symbol(i: InterpretedFct) = i match {
+    case Implies => "->"
+    case Or => "|"
+    case And => "&"
+    case Not => "!"
+    case Eq => "="
+    case Neq => "!="
+    case Geq => ">="
+    case Leq => "<="
+    case Gt => ">"
+    case Lt => "<"
+    case Plus => "+"
+    case Minus => "-"
+    case Times => "*"
+    case other => Logger.logAndThrow("princess", LogError, "not supported: " + other)
   }
 
   protected def asVar(str: String): String = {
@@ -44,41 +74,39 @@ object Printer {
     cstDecl("existentialConstants", vars)
   }
 
-  protected def printFormula(f: Formula)(implicit writer: BufferedWriter) = f match {
-    case Exists(vars, f2) => 
+  protected def printQuantifier(q: String, vars: Iterable[Variable], f: Formula)(implicit writer: BufferedWriter) {
+    writer.write("\\"+q+" int ")
+    writer.write(vars.map(asVar).mkString(", "))
+    writer.write("; ")
+    printFormula(f, priority(Not))
+  }
+
+  protected def printApplication(fct: InterpretedFct, args: List[Formula], priority1: Int = -1)(implicit writer: BufferedWriter) {
+    val priority2 = priority(fct)
+    val sym = symbol(fct)
+    if (priority2 <= priority1) writer.write("( ")
+    if (unary(fct)) {
+      Logger.assert(args.size == 1, "princess", "wrong arity for " + fct + ": " + args)
+      writer.write(sym + " ")
+      printFormula(args(0), priority2)
+    } else if (binary(fct)) {
+      Logger.assert(args.size == 2, "princess", "wrong arity for " + fct + ": " + args)
+      printFormula(args(0), priority2)
+      writer.write(" " + sym + " ")
+      printFormula(args(1), priority2)
+    } else {
+      Logger.assert(!args.isEmpty, "princess", "wrong arity for " + fct + ": " + args)
       sys.error("TODO")
-    case ForAll(vars, f2) => 
-      sys.error("TODO")
-    case v @ Variable(_) => 
-      sys.error("TODO")
-    case Literal(l) => 
-      sys.error("TODO")
-    case Application(Times, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Minus, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Plus, lst) =>
-      sys.error("TODO")
-    case Application(Eq, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Neq, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Leq, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Lt, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Geq, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Gt, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(Implies, a1 :: a2 :: Nil) =>
-      sys.error("TODO")
-    case Application(And, lst) =>
-      sys.error("TODO")
-    case Application(Or, lst) =>
-      sys.error("TODO")
-    case Application(Not, f2 :: Nil) =>
-      sys.error("TODO")
+    }
+    if (priority2 <= priority1) writer.write(" )")
+  }
+
+  protected def printFormula(f: Formula, currPriority: Int = -1)(implicit writer: BufferedWriter) = f match {
+    case Exists(vars, f2) => printQuantifier("exists", vars, f2)
+    case ForAll(vars, f2) => printQuantifier("forall", vars, f2)
+    case v @ Variable(_) => writer.write(asVar(v))
+    case Literal(l) => writer.write(l.toString)
+    case Application(fct: InterpretedFct, args) => printApplication(fct, args, currPriority)
     case other => Logger.logAndThrow("princess", LogError, "don't know what to do with " + other)
   }
   
