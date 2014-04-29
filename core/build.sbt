933db476b43f3ba3a307f095aa6714edbc6d2288
Damien Zufferey <zufferey@csail.mit.edu>
2014-04-29 14:47:47 -0400

    adding license, sat4j in build.sbt

--- core/build.sbt
+++ core/build.sbt
@@ -8,7 +8,10 @@ scalaVersion := "2.10.4"
 
 libraryDependencies ++=  Seq(
     "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
-    "org.apache.commons" % "commons-lang3" % "3.1"
+    "org.apache.commons" % "commons-lang3" % "3.1",
+    "org.ow2.sat4j" % "org.ow2.sat4j.core" % "2.3.4",
+    "org.ow2.sat4j" % "org.ow2.sat4j.pb" % "2.3.4",
+    "org.ow2.sat4j" % "org.ow2.sat4j.maxsat" % "2.3.4"
 )
 
 mainClass in (Compile, packageBin) := Some("picasso.frontend.dbpGraph.Main")
--- runGraph.sh
+++ runGraph.sh
@@ -3,6 +3,8 @@ export JAVA_OPTS="-Xmx1536M -Xms256M -Xss64M"
 scala_version1=scala-2.10
 scala_version2=scala-2.10.3
 BASEDIR=`dirname $0`
-cp="$HOME/.sbt/boot/$scala_version2/lib/*:$BASEDIR/core/target/${scala_version1}/classes:$HOME/.ivy2/cache/org.apache.commons/commons-lang3/jars/*:$BASEDIR/core/lib/*"
+scala="$HOME/.sbt/boot/$scala_version2/lib/*:$BASEDIR/core/target/${scala_version1}/classes"
+sat4j="$HOME/.ivy2/cache/org.ow2.sat4j/org.ow2.sat4j.core/jars/*:$HOME/.ivy2/cache/org.ow2.sat4j/org.ow2.sat4j.pb/jars/*:$HOME/.ivy2/cache/org.ow2.sat4j/org.ow2.sat4j.maxsar/jars/*"
+cp="$scala:$HOME/.ivy2/cache/org.apache.commons/commons-lang3/jars/*:$sat4j"
 
 exec java -cp ${cp} picasso.frontend.dbpGraph.Main $*
